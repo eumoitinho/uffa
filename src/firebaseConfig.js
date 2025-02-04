3c98ae0d7bbde3160ff8b027fafc37ee9b20ebd0
João Vitor <63624487+eumoitinho@users.noreply.github.com>
2025-02-04 01:16:23 -0300

    Update Firebase configuration to use environment variables and clean up .gitignore

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDe8UsqoUitF8Dto0w2X0ZG558YeFIFlrY",
-  authDomain: "uffa-expence-tracker-app.firebaseapp.com",
-  projectId: "uffa-expence-tracker-app",
-  storageBucket: "uffa-expence-tracker-app.appspot.com",
-  messagingSenderId: "148995235844",
-  appId: "1:148995235844:web:aa46075344159f4a879551",
-  measurementId: "G-GV0LRZBCWZ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
